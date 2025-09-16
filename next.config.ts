b443366675aa23577a462824e6385a86b81243f6
Aswanth Vc <aswanth.abam@gmail.com>
2025-09-16 23:56:56 +0530

    feat: change nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,43 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+    const SBI_EPAY_URL = process.env.NEXT_PUBLIC_SBI_EPAY_URL;
+
+    if (!API_BASE_URL || !SBI_EPAY_URL) {
+      console.error(
+        "Environment variables are not defined. Check your .env files."
+      );
+      throw new Error("Missing environment variables");
+    }
+
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              img-src 'self' data: https:;
+              font-src 'self' https://fonts.gstatic.com;
+              connect-src 'self' ${API_BASE_URL};
+              frame-ancestors 'self';
+              base-uri 'self';
+              form-action 'self' ${SBI_EPAY_URL};
+            `.replace(/\s{2,}/g, " "),
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
